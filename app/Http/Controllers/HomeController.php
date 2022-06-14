422d55fc474a0e88aa16c797123390b74cd953cb
bintangbind9 <bintangsaputra531@gmail.com>
2022-06-14 23:03:32 +0700

    Update dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use App\Helpers\Constant;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,6 +30,14 @@ class HomeController extends Controller
     public function index()
     {
         $section_header = 'Dashboard';
-        return view('home', compact('section_header'));
+        $posts = Auth::user()->hasRole(Constant::ROLE_ADMIN) ?
+            Post::orderBy('created_at','desc')->get() :
+            Post::where('user_id',Auth::user()->id)->orderBy('created_at','desc')->get();
+        $categories = Auth::user()->hasRole(Constant::ROLE_ADMIN) ?
+            Category::all() :
+            Category::where('user_id',Auth::user()->id)->get();
+        $users = User::all();
+        $roles = Role::all();
+        return view('home', compact('section_header','posts','categories','users','roles'));
     }
 }
